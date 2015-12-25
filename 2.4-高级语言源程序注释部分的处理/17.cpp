e20e689fcdb548723c6f1a0749c4b3fa01399d21
逗比丶张晓君 <610915518@QQ.com>
2015-12-25 13:57:35 +0800

    finish 2.4

--- 2.4-高级语言源程序注释部分的处理/17.cpp
+++ 2.4-高级语言源程序注释部分的处理/17.cpp
@@ -1,12 +1,4 @@
 
-//afdsughakjfshalsdfh
-
-/*
-1.11-xxx 24点游戏（难度等级：5级）
-【任务描述】
-任意给出4张牌，计算能否用＋、－、×、÷将其组合成24。
-输出其可能的组合式。
-*/
 
 #include<stdio.h>
 #include<time.h>
@@ -28,11 +20,11 @@ float calc(float n1, float n2, char o) {
 
 int i;
 
-int low=1;//下限为1
-int high=13;//上限为13
+int low=1;/下限为1
+int high=13;/上限为13
 
 char op[4]={'+','-','*','/'};
-float T=0;//T用来存放临时结果
+float T=0;/T用来存放临时结果
 
 float arr[4]={0};
 
@@ -59,7 +51,7 @@ void userGet(int arr[]) {
 
 	while(1)
 	{
-		sign=0;//输入检测
+		sign=0;/输入检测
 		
 		printf("请输入四张牌，每张牌之间用空格隔开，按回车键完成输入。\n输入样例：4 12 5 1 回车\n");
 		printf("请输入: ");
@@ -67,7 +59,7 @@ void userGet(int arr[]) {
 		fflush(stdin);
 		for(i=0;i<4;i++)
 			scanf("%5d",&arr[i]);
-		//这种写法在输入 ^ & ( ) 后会出现死循环 !@#$%^&...
+		/这种写法在输入 ^ & ( ) 后会出现死循环 !@#$%^&...
 	
 		printf("您输入的四张牌是: ");
 		for(i=0;i<4;i++)
@@ -102,11 +94,11 @@ void s_first() {
 int main(void)
 {
 	int pai[4]={0};
-	//float arr[4]={0};已经全局定义
+	/float arr[4]={0};已经全局定义
 	randomGet(pai);
-	//userGet(pai);
+	/userGet(pai);
 	for(i=0;i<4;i++)
-		arr[i]=(float)pai[i];//转换为浮点数组方便除法运算.避免用户输入浮点数
+		arr[i]=(float)pai[i];/转换为浮点数组方便除法运算.避免用户输入浮点数
 	system("pause");
 
 
--- 2.4-高级语言源程序注释部分的处理/annotation-processing.cpp
+++ 2.4-高级语言源程序注释部分的处理/annotation-processing.cpp
@@ -59,34 +59,153 @@ int main(void) {
 		quit = 0; //读取成功了...下次不再进行死循环
 	}
 
-	char first[MAX_READ];
+	char first[MAX_READ];//first用来存放文件数据
+	memset(first,0,sizeof(char)*MAX_READ);
 	printf("正在读取 %s ...\n",name);
 
 	for (i=0; (!feof(fp))&&i<MAX_READ; i++) {
 
-		if(fread(first, sizeof(char), 1, fp)!=1) {
+		if(fread(first+i, sizeof(char), 1, fp)!=1) {
             printf("共计读取%d个字符\n",i+1);
 		}
 	}
 	fclose(fp);
 	
+
+
+/****************************************************************/
+
+
 	FILE * second; //second用来存放源代码
 
-	printf("请输入要存放源代码的文件名...默认后缀: *.cpp\n");
+	quit = 1;
+	char name_1[70];
+	memset(name_1,0,sizeof(char)*70);
 
-	if ((fp = fopen(, "w")) == NULL) {
+	while (quit) {
 
-			printf("文件没有找到 %s ,读取文件失败\n",name);
-			printf("是否重新输入文件名? y/n ? ");
-			fflush(stdin);
-			if( (ch=getchar())==124)
-				continue;
-			else
-				quit=0;
+		printf("请输入要存放源代码的文件名...默认后缀: *.cpp\n");
+		scanf("%50s",name_1);
+		
+		printf("您输入的是: ");
+		j=printf("%s",name_1);
+		printf("\n");
+		if(j==0) {
+			printf("输入为空,请重新输入!\n");
+			continue;
+		}
+		for(i=0;i<j;i++)
+			if(name_1[i]=='.')
+				break;
+		if(i==j)
+			strcpy(&name_1[i],".cpp");
+		
+		if ((second = fopen(name_1, "w")) == NULL) {
+
+			printf("文件 %s 创建或读取文件失败\n请检查当前用户权限!\n",name_1);
+			exit(-2);
+		}
+		
+		quit=0;
+		
+	}
+
+
+/****************************************************************/
+
+
+
+	FILE * third; //third用来存放注释部分
+
+	quit=1;
+	char name_2[70];
+	memset(name_2,0,sizeof(char)*70);
+
+	while (quit) {
+
+	printf("请输入要存放注释的文件名...默认后缀: *.txt\n");
+	scanf("%50s",name_2);
+	
+		printf("您输入的是: ");
+		j=printf("%s",name_2);
+		printf("\n");
+		if(j==0) {
+			printf("输入为空,请重新输入!\n");
+			continue;
+		}
+		for(i=0;i<j;i++)
+			if(name_2[i]=='.')
+				break;
+		if(i==j)
+			strcpy(&name_2[i],".txt");
+		
+		if ((third = fopen(name_2, "w")) == NULL) {
+
+			printf("文件 %s 创建或读取文件失败\n请检查当前用户权限!\n",name_2);
+			exit(-2);
 		}
 
+		quit=0;
+	
+	}
+	
+
+/****************************************************************/
 
 
+	//开始输出源代码到name_1.cpp
+	
+	int sign = 0;
+	int count = 0;
+	char * p = NULL;
+	char * q = NULL;
+	p = first;
+	q = first+1;
+
+	while (*p!='\0') {
+
+		if (*p=='/'&&*q=='/') {
+
+			for(;;p++,q++,count++) {
+
+				//fwrite(p,sizeof(char),1,third);
+				fputc(*p,third);
+				if(*p=='\n'||*p=='\0')
+					break;
+
+			}
+
+		} else if (*p=='/'&&*q=='*') {
+
+			for(;*p!='\0';p++,q++,count++) {
+
+				fputc(*p,third);
+
+				if(*p=='*'&&*q=='/') {
+
+					//fwrite(p,sizeof(char),1,third);
+					//fwrite(q,sizeof(char),1,third);
+					fputc(*q,third);
+					fputc('\n',third);
+					p++;
+					q=p+1;
+					break;
+				}
+
+			}
+			
+		} else
+			//fprintf(second,"%c",p);
+			fputc(*p,second);
+
+		p++;
+		q++;
+	}
+
+
+
+	fclose(second);
+	fclose(third);
 
 	return 0;
 }
