c3ba72a5d0eb3f1c7a4eff8e1492da250bb0ecbb
逗比丶张晓君 <610915518@QQ.com>
2015-12-29 23:50:02 +0800

    2015-12-29 23:49:54

--- 1.11-24点游戏的子程序-支持括号的简单计算器/simpleCalculator.cpp
+++ 1.11-24点游戏的子程序-支持括号的简单计算器/simpleCalculator.cpp
@@ -1,104 +1,101 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
-char token;
-void match(char expectedToken) /*对当前的标志进行匹配*/
-{
-	if(token==expectedToken) token=getchar(); /*匹配成功，获取下一个标志*/
-	else 
-	{       
-		printf("cannot match\n");
-		exit(1); /*匹配不成功，退出程序*/
-	}
+#include <string.h>
+
+char op[4]={'+','-','*','/'};
+
+
+float calc(float n1, float n2, char o) {
+
+    switch(o) {
+        case '+': return (n1+n2);
+        case '-': return (n1-n2);
+        case '*': return (n1*n2);
+        case '/': 
+			if(n2==0)
+				return -30000;
+			else
+				return (n1/n2);
+		default: exit(-1);
+    }
 }
 
-int main()
-{   
-	int result;  /*运算的结果*/
-	int low();
-	printf("*****************************************\n");
-	printf("**Welcome to use this simple calculator**\n"); 
-	printf("**Please input a multinomial like      **\n");
-	printf("**                    6-3*(5-1)/2+14/7 **\n");
-	printf("*****************************************\n");
-	token = getchar(); /*载入第一个符号*/
-    
-	result = low(); /*进行计算*/
-	if( token == '\n' ) /* 是否一行结束 */
-		printf( "The answer is : %d\n", result );
-	else 
-	{
-		printf( "Unexpected char!");
-		exit(1); /* 出现了例外的字符 */
-	}
-	scanf("%d",result);
+int find(char *p) {
+
+	int i=0;
+	while(*p!='\0'&&*p!='='&&i<2)
+		if(*p=='(')
+			return 1;
+		else if(*p=='+'||*p=='-'||*p=='*'||*p=='/'||*p==')')
+			i++,p++;
+		else
+			p++;
 	return 0;
 }
-int low(void)/*用于计算表达式中级别最低的运算*/
-{
-	int mid();
-	int result=mid(); /*计算比加减运算优先级别高的部分*/
-	while((token=='+')||(token=='-'))
-		if (token=='+')
-		{
-			match('+');     /*进行加法运算*/
-			result+=mid();
-			break;
-		}
-		else if (token=='-')
-		{
-			match('-');    /*进行减法运算*/
-			result-=mid(); 
-			break;
-		}
-		return result;
-}
-int mid(void)
-{
-	int div;
-	int high();
-	int result=high();
-	while((token=='*')||(token=='/'))
-		if(token=='*')
-		{
-			match('*');
-			result *=high();
-			break;
-		}
-		else if (token =='/')
-		{
-			match('/');
-			div=high();
-			if(div==0)
-			{
-				printf("除数为零.\n");
-				exit(1);
+
+float sum(char *p,float result) {
+
+	//9*(3+6*(7+8))=
+	float a,b;
+	do {
+		if(*p=='(') {
+			if(*(++p)=='(') {
+				result=sum(p+1,result);
+				while(*p!=')') p++;
+				p++;
+			}else{
+				result=sum(p,result);
+				while(*p!=')') p++;
+				p++;
 			}
-			result/=div;
-			break;
 		}
+		else if(*p>='0'&&*p<='9') {
+
+			a=(float)atof(p);
+			while( (*p>='0'&&*p<='9')||*p=='.') p++;
+			if(*(p+1)=='(') {
+				result=calc(a,sum(p+2,result),*p)				;
+				return result;
+			}else{
+				b=(float)atof(p);
+				result=calc(a,b,*p);
+				return result;
+			}
+		}
+		else if(*p=='+'||*p=='-'||*p=='*'||*p=='/'||*p==')') {
+
+			if(*p==')')
+				p++;
+
+			if(find(p)) {//后面有'('
+				char ch=*p;
+				while(*p!='(') p++; //p指向下一个'('
+				result=calc(result,sum(p,0),ch);
+				while(*p!=')') p++; p++; //p跳到下一个')'
+			}else{
+				b=(float)atof(p+1);
+				result=calc(result,b,*p);
+				p++;
+				while( (*p>='0'&&*p<='9')||*p=='.') p++;
+			}
+		}else
+			return result;
+	}while(*p!='=');
+
 	return result;
+
 }
-int high( void )/*用于计算表达式中级别最高的运算，即带（）的运算*/
-{
-	int result;
-	int low();
-	if(token=='(') /*带有括号的运算*/
-	{
-		match('(');
-		result=low();/*递归计算表达式*/
-		match(')');
-	}
-	else if (token>='0'&&token<='9') /*实际的数字*/
-	{
-		ungetc(token,stdin); /*将读入的字符退还给输入流，为读取整个数*/
-		scanf("%d",&result); /*读出数字*/
-		token=getchar();  /*读出当前的标志*/
-	}
-	else
-	{
-		printf("The input has unexpected char\n"); /*不是括号也不是数字*/
-		exit(1);
-	}
-	return result;
+
+int main(void) {
+
+	char arr[30];
+	memset(arr,0,sizeof(char)*30);
+	scanf("%s",arr);
+
+	float b;
+	b=sum(arr,0);
+	printf("%g\n",b);
+
+	return 0;
 }
+
